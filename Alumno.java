e248032fbc3008fbd3d0b620e168557e9a4784d6
Roberto <robertoabad93@gmail.com>
2020-11-19 13:27:11 +0100

    3333.02 en el metodo se han puesto varias condiciones para que no de fallo

--- Alumno.java
+++ Alumno.java
@@ -1,5 +1,5 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
@@ -12,13 +12,7 @@ public class Alumno {
      */
     public Alumno(String nombreCompleto, String numeroMatriculaAlumno, int edadAlumno) {
         nombre = nombreCompleto;
-        if (nombre.length() < 3){
-            System.out.println("Nombre demasiado corto minimo 3 caracteres");
-        }
         numeroMatricula = numeroMatriculaAlumno;
-        if(numeroMatricula.length() < 4){
-            System.out.println("Numero Matricula demasiado corto minimo 4 caracteres");
-        }
         edad = edadAlumno;
     }
 
@@ -28,27 +22,42 @@ public class Alumno {
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
         System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " años");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en su cuenta de Github en formato de 7 caracateres
      */
     public String getNombreUsuarioGithub() {
-        return nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
+        if (nombre.length() >= 3 ){
+            if(numeroMatricula.length() >=4){
+                return nombre.substring(0, 3) + numeroMatricula.substring(0, 4);
+            }
+            else{
+                return nombre.substring(0, 3) + numeroMatricula;
+            }
+        }
+        else{
+            if(numeroMatricula.length() >=4){
+                return nombre + numeroMatricula.substring(0, 4);  
+            }
+            else{
+                return nombre + numeroMatricula;
+            }
+
+        }
     }
-    
 }
